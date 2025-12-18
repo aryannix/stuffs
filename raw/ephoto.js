760ff3765d97fb7c770ba7c8a786319b98ebe19e
Aryan Rayhan <aryanchowdhury439@yahoo.com>
2025-12-18 16:25:41 +0600

    Create ephoto.js

---
+++ raw/ephoto.js
@@ -0,0 +1,9 @@
+{
+  "handwriting_foggy_glass": "https://en.ephoto360.com/handwritten-text-on-foggy-glass-online-680.html",
+  "neon_light_text": "https://en.ephoto360.com/neon-light-text-effect-online-882.html", 
+  "blackpink_style": "https://en.ephoto360.com/blackpink-style-logo-maker-online-711.html",
+  "glitch_text": "https://en.ephoto360.com/create-digital-glitch-text-effect-online-767.html",
+  "gold_text": "https://en.ephoto360.com/free-gold-text-effect-online-683.html",
+  "3d_text": "https://en.ephoto360.com/3d-text-effect-online-970.html",
+  "write_sand": "https://en.ephoto360.com/write-name-on-sand-summer-beach-online-576.html"
+}
